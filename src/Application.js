b9f2510dbb29aa1930a4e23d98d180a04d314a9c
Diego Krefta <diego.krefta@gmail.com>
2018-08-24 16:35:52 -0300

    Stateless

--- src/Application.js
+++ src/Application.js
@@ -10,6 +10,34 @@ const initialState = {
   numberOfPeople: 10,
   slicesPerPerson: 2,
 };
+class PizzaCalculator extends Component {
+  render () {
+    const {numberOfPeople, numberOfPizzas,  updateNumberOfPeople, slicesPerPerson, updateSlicesPerPerson, reset} = this.props
+    return (
+      <div className="Application">
+        <Title />
+        <Input
+          label="Number of Guests"
+          type="number"
+          min={0}
+          value={numberOfPeople}
+          onChange={updateNumberOfPeople}
+        />
+        <Input
+          label="Slices Per Person"
+          type="number"
+          min={0}
+          value={slicesPerPerson}
+          onChange={updateSlicesPerPerson}
+        />
+        <Result amount={numberOfPizzas} />
+        <button className="full-width" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    )
+  }
+}
 
 export default class Application extends Component {
   state = { ...initialState };
@@ -36,27 +64,16 @@ export default class Application extends Component {
     );
 
     return (
-      <div className="Application">
-        <Title />
-        <Input
-          label="Number of Guests"
-          type="number"
-          min={0}
-          value={numberOfPeople}
-          onChange={this.updateNumberOfPeople}
-        />
-        <Input
-          label="Slices Per Person"
-          type="number"
-          min={0}
-          value={slicesPerPerson}
-          onChange={this.updateSlicesPerPerson}
-        />
-        <Result amount={numberOfPizzas} />
-        <button className="full-width" onClick={this.reset}>
-          Reset
-        </button>
-      </div>
+      <PizzaCalculator 
+      numberOfPeople={numberOfPeople}
+      updateNumberOfPeople={this.updateNumberOfPeople}
+      slicesPerPerson={slicesPerPerson}
+      updateSlicesPerPerson={this.updateSlicesPerPerson}
+      numberOfPizzas={numberOfPizzas}
+      reset={this.reset}
+      
+      />
+
     );
   }
 }
